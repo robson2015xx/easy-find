cc8f7cf932709159c46976fe6f71214c0dd3b7da
Robson Teixeira <robson2015xx@gmail.com>
2022-12-24 23:07:27 -0300

    initial commit

---
+++ src/main/java/com/rtech/easyfind/EasyFindApplication.java
@@ -0,0 +1,13 @@
+package com.rtech.easyfind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyFindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyFindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rtech/easyfind/EasyFindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtech.easyfind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyFindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
